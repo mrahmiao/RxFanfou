5b0be7fa4eb948c7162d77c5237c2693e20a121a
mrahmiao <mrahmiao@gmail.com>
2016-04-22 19:57:16 +0800

    首次提交
    Signed-off-by: mrahmiao <mrahmiao@gmail.com>

---
+++ Supporting Files/FanfouAPI.h
@@ -0,0 +1,19 @@
+//
+//  FanfouAPI.h
+//  FanfouAPI
+//
+//  Created by mrahmiao on 4/22/16.
+//  Copyright © 2016 EWStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FanfouAPI.
+FOUNDATION_EXPORT double FanfouAPIVersionNumber;
+
+//! Project version string for FanfouAPI.
+FOUNDATION_EXPORT const unsigned char FanfouAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FanfouAPI/PublicHeader.h>
+
+
---
+++ Supporting Files/RxFanfouAPI.h
@@ -0,0 +1,19 @@
+//
+//  RxFanfouAPI.h
+//  RxFanfouAPI
+//
+//  Created by mrahmiao on 4/22/16.
+//  Copyright © 2016 EWStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxFanfouAPI.
+FOUNDATION_EXPORT double RxFanfouAPIVersionNumber;
+
+//! Project version string for RxFanfouAPI.
+FOUNDATION_EXPORT const unsigned char RxFanfouAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxFanfouAPI/PublicHeader.h>
+
+
