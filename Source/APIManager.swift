62ce6fcc842c19dc274105e931c3beafe821cb70
mrahmiao <mrahmiao@gmail.com>
2016-04-22 20:42:10 +0800

    增加APIManager以及API的命名空间struct
    Signed-off-by: mrahmiao <mrahmiao@gmail.com>

---
+++ Source/APIManager.swift
@@ -0,0 +1,18 @@
+//
+//  APIManager.swift
+//  RxFanfouAPI
+//
+//  Created by mrahmiao on 4/22/16.
+//  Copyright © 2016 EWStudio. All rights reserved.
+//
+
+import Foundation
+
+public struct APIManager {
+
+}
+
+/**
+ *  Fanfou API的命名空间
+ */
+struct API { }
