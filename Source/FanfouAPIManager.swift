d799c498e8934f5eed56b2bf98016c869933533f
mrahmiao <mrahmiao@gmail.com>
2016-04-23 12:55:00 +0800

    增加FoufanAPIManager的文档
    Signed-off-by: mrahmiao <mrahmiao@gmail.com>

--- Source/FanfouAPIManager.swift
+++ Source/FanfouAPIManager.swift
@@ -13,18 +13,27 @@ import Log
 let logger = Logger()
 
 /**
- *  SDK入口
+ *  FanfouAPI管理器，整个SDK的入口
  */
 public struct FanfouAPIManager {
 
   private let consumerCredential: ConsumerCredential
   private let authorizationType: AuthorizationType
 
+  /**
+   使用`ConsumerCredential`以及`AuthorizationType`来构造一个FanfouAPI管理器
+
+   - parameter credential:        饭否第三方应用的Token
+   - parameter authorizationType: 授权类型，默认为.Mobile
+
+   - returns: 饭否API管理器
+   */
   public init(credential: ConsumerCredential, authorizationType: AuthorizationType = .Mobile) {
     self.consumerCredential = credential
     self.authorizationType = authorizationType
   }
 
+  /// 通过`OAuth`调用授权相关的API
   public private(set) lazy var OAuth: OAuthManager = {
     return OAuthManager(credential: self.consumerCredential, authorizationType: self.authorizationType)
   }()
