69754e73b26b271b0c02a559748af5ec8397d116
mrahmiao <mrahmiao@gmail.com>
2016-05-18 16:39:42 +0800

    增加Account相关接口的Rx接口
    Signed-off-by: mrahmiao <mrahmiao@gmail.com>

--- Source/RxSwift/AccountManager+Rx.swift
+++ Source/RxSwift/AccountManager+Rx.swift
@@ -28,4 +28,37 @@ extension AccountManager {
       }
     }
   }
+
+  public func getNotifications<T>(reformer: [String: AnyObject] -> T?) -> Observable<T> {
+    return Observable.create { [weak self] observer in
+
+      let cancellableToken = self?.getNotifications(reformer, completion: onCompletion(observer))
+
+      return AnonymousDisposable {
+        cancellableToken?.cancel()
+      }
+    }
+  }
+
+  public func updateProfile<T>(profile: TargetProfile, reformer: [String: AnyObject] -> T?) -> Observable<T> {
+    return Observable.create { [weak self] observer in
+
+      let cancellableToken = self?.updateProfile(profile, reformer: reformer, completion: onCompletion(observer))
+
+      return AnonymousDisposable {
+        cancellableToken?.cancel()
+      }
+    }
+  }
+
+  public func updateProfileImage<T>(imageData: NSData, reformer: [String: AnyObject] -> T?) -> Observable<T> {
+    return Observable.create { [weak self] observer in
+
+      let cancellableToken = self?.updateProfileImage(imageData: imageData, reformer: reformer, completion: onCompletion(observer))
+
+      return AnonymousDisposable {
+        cancellableToken?.cancel()
+      }
+    }
+  }
 }
